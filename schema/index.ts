981f421809f0393e64675159bbde19ba9b8a1786
Rohithegade02 <rohithegade8@gmail.com>
2026-02-21 18:42:31 +0530

    docs: Add comments to the register and login schema definitions.

--- schema/index.ts
+++ schema/index.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
+// Register Schema
 export const registerSchema = z.object({
     username: z.string().min(3, 'Username must be at least 3 characters').toLowerCase(),
     email: z.email('Invalid email address'),
@@ -7,6 +8,7 @@ export const registerSchema = z.object({
     role: z.enum(['ADMIN', 'USER']),
 });
 
+// Login Schema
 export const loginSchema = z.object({
     username: z.string().min(3, 'Username must be at least 3 characters'),
     password: z.string().min(6, 'Password must be at least 6 characters'),
